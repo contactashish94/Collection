26f1d406094b0e860a4a9a5a5dd1e06e44ea2dea
ashishku <ashishku@NB1499-R90WLR25.lowes.com>
2021-02-22 11:02:12 +0530

    added Theatre class

--- src/com/ashish/buchalka/collection/Main.java
+++ src/com/ashish/buchalka/collection/Main.java
@@ -3,8 +3,35 @@ package com.ashish.buchalka.collection;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Theatre theatre= new Theatre("PVR", 8, 12);
+		
+		theatre.getSeats();
+		
+		if(theatre.reserveSeat("H11")){
+			System.out.println("Please Proceed for payment pay");
+		} else {
+			System.out.println("seat already reserved");
+		}
+		if(theatre.reserveSeat("H11")){
+			System.out.println("Please Proceed for payment pay");
+		} else {
+			System.out.println("Sorry, seat already reserved!");
+		}
+		
+		theatre.cancelSeat("H11");
+		
+		if(theatre.reserveSeat("H11")){
+			System.out.println("booked after cancellation - Please Proceed for payment pay");
+		} else {
+			System.out.println("seat already reserved");
+		}
+		if(theatre.reserveSeat("H11")){
+			System.out.println("Please Proceed for payment pay");
+		} else {
+			System.out.println("seat already reserved");
+		}
+		
 	}
 
 }
